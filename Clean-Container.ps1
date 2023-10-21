7cd51ddcdd8247f5550600afe50acc853ab12c7e
Kasper Johansen <kaspersmjohansen@gmail.com>
2023-10-21 20:23:34 +0200

    Initial commit

---
+++ Clean-Container.ps1
@@ -0,0 +1,38 @@
+﻿param(
+      [Parameter(Mandatory = $true)]
+      [ValidateScript( { If (Test-Path -Path $_ -PathType 'Leaf') { $True } Else { Throw "Cannot find log file directory: $_." } })]
+      [string]$FsLogixRedirFile,
+      [string]$LogPath = $env:USERPROFILE,
+      [switch]$WhatIf
+     )
+
+function Clean-Container
+    {
+        # Start logging
+        $LogPS = $LogPath + "\FSLogix Profile Cleanup.log"
+        Start-Transcript -Path $LogPS
+
+        # Get FSLogix Redirection.xml file content
+        [xml]$RedirXML = Get-Content -Path $FsLogixRedirFile
+        $ExcludedFolders = $RedirXML.FrxProfileFolderRedirection.Excludes.Exclude."#text"
+
+        # Match content of FSLogix Redirection.xml file, and delete folders recursively
+        If ($WhatIf)
+        {
+            $WhatIfPreference = $true
+        }
+                ForEach ($folder in $ExcludedFolders)
+                {
+                    If (Test-Path -Path "$env:USERPROFILE\$folder")
+                    {
+                        Remove-Item -Path "$env:USERPROFILE\$folder" -Recurse -Force -Verbose -ErrorAction SilentlyContinue
+                    }
+                }
+
+        # Stop logging
+        Stop-Transcript
+    }
+
+Clean-Container $FsLogixRedirFile $LogPath $WhatIf
+
+[void][System.Console]::ReadKey($FALSE)
---
+++ FSLOgix-dynamic-redirections.xml.ps1
@@ -0,0 +1,67 @@
+﻿function buildlist($b, $p)
+{
+	if ($b -eq "Chrome")
+	{
+		$folders = "Cache", "Cached Theme Image", "JumpListIcons", "JumpListIconsOld", "Storage", "Local Storage", "Session Storage", "Storage", "GPUCache", "Sync Data", "blob_storage", "IndexedDB", "Application Cache", "Code Cache"
+		foreach ($folder in $folders)
+		{
+			if (test-path (Join-Path $p $folder))
+			{
+				((join-path $p $folder) -split "$env:username\\")[1]
+			}
+		}
+	}
+	else
+	{
+		$folders = "cache2", "jumpListCache", "OfflineCache", "startupCache", "thumbnails"
+		foreach ($folder in $folders)
+		{
+			if (test-path (Join-Path $p $folder))
+			{
+				((join-path $p $folder) -split "$env:username\\")[1]
+			}
+		}
+	}
+	
+}
+
+$redirection = "$env:localappdata\FSLogix\Redirections.xml"
+$firefoxprofiles = (gci "$env:localappdata\mozilla\firefox\Profiles" | ?{ $_.psiscontainer }).fullname
+$firefoxexcludes = @()
+$chromeexcludes = @()
+foreach ($fprof in $firefoxprofiles)
+{
+	$firefoxexcludes += buildlist -b firefox -p $fprof
+}
+$googleprofiles = (gci "$env:localappdata\Google\Chrome\User Data" | ?{ $_.psiscontainer -and ($_.name -eq "default" -or $_.name -like "profile*") }).fullname
+foreach ($gprof in $googleprofiles)
+{
+	$chromeexcludes = buildlist -b chrome -p $gprof
+}
+$template = "<?xml version=`"1.0`"?>
+<FrxProfileFolderRedirection ExcludeCommonFolders=`"128`">
+	<Includes>
+		<Include>AppData\Roaming\Microsoft\Excel\XLSTART\</Include>
+        <Include>AppData\Roaming\Microsoft\Word\STARTUP</Include>
+	</Includes>
+	<Excludes>
+		<Exclude Copy=`"0`">AppData\Roaming\Microsoft\Excel\</Exclude>
+        <Exclude Copy=`"0`">AppData\Roaming\Microsoft\Word\</Exclude>"
+$end = "`r`n</Excludes>
+</FrxProfileFolderRedirection>"
+if ($firefoxexcludes)
+{
+	foreach ($f in $firefoxexcludes)
+	{
+		$template += "`r`n<Exclude Copy=`"0`">" + $f + "</Exclude>"
+	}
+}
+if ($chromeexcludes)
+{
+	foreach ($c in $chromeexcludes)
+	{
+		$template += "`r`n<Exclude Copy=`"0`">" + $c + "</Exclude>"
+	}
+}
+$template += $end
+$template | out-file $redirection -force
